06e6c03c917151395531b20b0f6f635142dbd9c9
Nicetiesniceties <jonjoncom58@gmail.com>
2020-10-16 19:43:50 +0800

    Added prediction log and its function

--- newWebsite/js/demo.js
+++ newWebsite/js/demo.js
@@ -10,6 +10,93 @@ function putImage()
   //imageElement.src = myImage;                           
   alert("it's " + parseInt((Math.random() * Math.floor(9) % 10)));
 }  
+
+async function showSingleExample(imageData) {
+  // Create a container in the visor
+  const surface =
+    tfvis.visor().surface({ name: 'User Input Log', tab: 'Input Data'});  
+    //console.log(surface)
+  
+  // Create a canvas element to render each example
+  // const imageTensor = tf.tidy(() => {
+  //   // Reshape the image to 28x28 px
+  //   return examples.xs
+  //     .slice([i, 0], [1, examples.xs.shape[1]])
+  //     .reshape([28, 28, 1]);
+  // });
+    
+  const canvas = document.createElement('canvas');
+  canvas.width = 28;
+  canvas.height = 28;
+  canvas.style = 'margin: 4px;';
+  await tf.browser.toPixels(imageData, canvas);
+  surface.drawArea.appendChild(canvas);
+
+  imageData.dispose();
+}
+
+function resize(imageData){
+  var tfImage = tf.browser.fromPixels(imageData, 1);
+  console.log("origin", tfImage.dataSync());
+
+  //Resize to 28X28
+  var tfResizedImage = tf.image.resizeBilinear(tfImage, [28,28]);
+  console.log("bilinear", tfResizedImage.dataSync());
+  showSingleExample(tfResizedImage)
+
+  //Since white is 255 black is 0 so need to revert the values
+  //so that white is 0 and black is 255
+  // tfResizedImage = tf.cast(tfResizedImage, 'float32');
+  // console.log("cast", tfResizedImage.dataSync());
+  // showSingleExample(tfResizedImage.reshape([28, 28]))
+
+  // tfResizedImage = tf.abs(tfResizedImage.sub(tf.scalar(255)))
+  //     .div(tf.scalar(255)).flatten();
+  // showSingleExample(tfResizedImage.reshape([28, 28]))
+
+  // console.log("abs", tfResizedImage.dataSync());
+  tfResizedImage = tfResizedImage.reshape([1, 784]);
+  return tfResizedImage;
+}
+
+async function predict(){
+  const cors = 'https://cors-anywhere.herokuapp.com/'; // use cors-anywhere to fetch api data
+  //const model = await tf.loadLayersModel(cors + 'ai.ntu.edu.tw/resource/model/my-model-1.json');
+  const model = await tf.loadLayersModel('https://s3-ap-southeast-2.amazonaws.com/mystaticdemo/model.json');
+  //var canvas = document.getElementById('myCanvas');
+  var ctx = document.getElementById('myCanvas').getContext('2d');
+  //await model.save('downloads://my-model-1');
+  // console.log(ctx.getImageData(0,0, 100, 100));
+  console.log(ctx);
+  var imageData = document.getElementById('myCanvas').getContext('2d').getImageData(0, 0, 600, 400);
+  //ctx.putImageData(imageData, 10, 70);
+  
+  console.log(imageData);
+
+  
+  let tfResizedImage = resize(imageData);
+  //Make another dimention as the model expects
+  console.log(tfResizedImage.dataSync());
+
+  var output = model.predict(tfResizedImage); //[1, 28, 28, 1]
+  var result = Array.from(output.dataSync());
+  console.log('Output is : ', Array.from(output.dataSync()));
+  var maxPossibility = result.reduce(function(a,b){return Math.max(a,b)});
+  console.log(maxPossibility);
+  alert(result.indexOf(maxPossibility) );
+
+  //let response = await fetch('http://ai.ntu.edu.tw/resource/model/my-model-1.json');
+  //let response = await fetch(cors + 'ai.ntu.edu.tw/resource/model/my-model-1.json'); 
+  // if (response.ok) { // if HTTP-status is 200-299
+  //   // get the response body (the method explained below)
+  //   console.log("Model loaded!");
+  //   let json = await response.json();
+  // } else {
+  //   alert("HTTP-Error: " + response.status);
+  // }
+
+}
+
 function clearCanvas()
 {
   var canvas = document.getElementById("canvas");   
@@ -59,3 +146,25 @@ function cosineSimilarity(vec1, vec2) {
 function calcVectorSize(vec) {
   return Math.sqrt(vec.reduce((accum, curr) => accum + Math.pow(curr, 2), 0));
 };
+
+
+// const recorder = document.getElementById('recorder');
+// const player = document.getElementById('player');
+
+// recorder.addEventListener('change', function(e) {
+//   const file = e.target.files[0];
+//   const url = URL.createObjectURL(file);
+//   // Do something with the audio file.
+//   player.src = url;
+// });
+
+// const handleSuccess = function(stream) {
+//   if (window.URL) {
+//     player.srcObject = stream;
+//   } else {
+//     player.src = stream;
+//   }
+// };
+
+// navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+//     .then(handleSuccess);
--- newWebsite/js/draw.js
+++ newWebsite/js/draw.js
@@ -1,91 +1,8 @@
-// // wait for the content of the window element 
-// // to load, then performs the operations. 
-// // This is considered best practice. 
-// window.addEventListener('load', ()=>{ 
-//     resize(); // Resizes the canvas once the window loads 
-//     document.addEventListener('mousedown', startPainting); 
-//     document.addEventListener('mouseup', stopPainting); 
-//     document.addEventListener('mousemove', sketch); 
-//     window.addEventListener('resize', resize); 
-// }); 
-    
-// const canvas = document.querySelector('#canvas'); 
-   
-// // Context for the canvas for 2 dimensional operations 
-// const ctx = canvas.getContext('2d'); 
-
-// // Resizes the canvas to the available size of the window. 
-// function resize(){ 
-//   ctx.canvas.width = 400; 
-//   ctx.canvas.height = 400; 
-// } 
-    
-// // Stores the initial position of the cursor 
-// let coord = {x:0, y:0};  
-   
-// // This is the flag that we are going to use to  
-// // trigger drawing 
-// let paint = false; 
-    
-// // Updates the coordianates of the cursor when  
-// // an event e is triggered to the coordinates where  
-// // the said event is triggered. 
-// function getPosition(event){ 
-//   coord.x = event.clientX - 0// - canvas.offsetLeft; 
-//   coord.y = event.clientY - 30// - canvas.offsetTop; 
-
-//   console.log("Client", event.clientX, event.clientY)
-//   console.log("Canvas", canvas.offsetLeft, canvas.offsetTop)
-//   console.log("Coord", coord.x, coord.y)
-//   console.log("-------")
-// } 
-  
-// // The following functions toggle the flag to start 
-// // and stop drawing 
-// function startPainting(event){ 
-//   paint = true; 
-//   getPosition(event); 
-// } 
-// function stopPainting(){ 
-//   paint = false; 
-// } 
-    
-// function sketch(event){ 
-//   if (!paint) return; 
-//   ctx.beginPath(); 
-    
-//   ctx.lineWidth = 5; 
-   
-//   // Sets the end of the lines drawn 
-//   // to a round shape. 
-//   ctx.lineCap = 'round'; 
-    
-//   ctx.strokeStyle = 'black'; 
-      
-//   // The cursor to start drawing 
-//   // moves to this coordinate 
-//   ctx.moveTo(coord.x, coord.y); 
-   
-//   // The position of the cursor 
-//   // gets updated as we move the 
-//   // mouse around. 
-//   getPosition(event); 
-   
-//   // A line is traced from start 
-//   // coordinate to this coordinate 
-//   ctx.lineTo(coord.x , coord.y); 
-    
-//   // Draws the line. 
-  
-//   ctx.stroke(); 
-// } 
-
-
 var mousePressed = false;
 var lastX, lastY;
 var ctx;
 
-function InitThis() {
+function InitDraw() {
     ctx = document.getElementById('myCanvas').getContext("2d");
 
     $('#myCanvas').mousedown(function (e) {
@@ -109,9 +26,10 @@ function InitThis() {
 
 function Draw(x, y, isDown) {
     if (isDown) {
+        ctx = document.getElementById('myCanvas').getContext("2d");
         ctx.beginPath();
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 2;
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 30;
         ctx.lineJoin = "round";
         ctx.moveTo(lastX, lastY);
         ctx.lineTo(x, y);
